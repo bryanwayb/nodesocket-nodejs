ce9c4654e50d4f879c729631cfea41058823a801
Bryan Way <bryanwayb@gmail.com>
2015-06-04 02:54:42 -0500

    Added error handling for the client and server

--- test/client.js
+++ test/client.js
@@ -4,10 +4,14 @@ var client = nodesocket().createClient(22, '127.0.0.1'); // Connect to 127.0.0.1
 
 var example = client.func('example'); // Create a JavaScript function to call the 'example' function on the server.
 
+client.on('error', function(error, socket) {
+	console.log(error);
+});
+
 client.on('verified', function(socket) { // Signals that we can now send functions to be executed.
 	example(function(results) {
 		console.log('Response Received: ' + results);
-		client.close(); // Nicely shutdown the client.
+		//client.close(); // Nicely shutdown the client.
 	}, 'hello');
 });
 
--- test/server.js
+++ test/server.js
@@ -2,11 +2,15 @@ var nodesocket = require('../lib/index.js');
 
 var server = nodesocket().createServer(22, '127.0.0.1'); // Hosts the server with the IP 127.0.0.1 on port 22
 
+server.on('error', function(error, client, server) {
+	console.log(error);
+});
+
 server.func('example', function(s) { // Register a function called 'example'
 	console.log('Hello, World. This is being executed on the server.\nParameter: ' + s);
 	console.log('Giving okay to stop server connections and begin shutting down.');
-	server.close();
-	return 'The was returned from the server';
+	//server.close();
+	return 'This was returned from the server';
 });
 
 server.start();
